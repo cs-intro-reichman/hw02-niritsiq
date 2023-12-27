9eaa18966d9f5997e0b173241a45ac7ff2397cad
unknown <nir.itsiq@post.runi.ac.il>
2023-12-27 17:44:02 +0200

    'hw'

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,11 +5,11 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		//// Put your code here
 		int a = Integer.parseInt(args[0]);
-		boolean check = true;
+
 		for (int i = 0; i < a; i++) {
-			check = true;
+
 			for (int j = 0; j < a; j++) {
-				if (i % 2 == 1 && check) {
+				if (i % 2 == 1) {
 
 					if (j == 0)
 						System.out.print(" * ");
@@ -18,7 +18,7 @@ public class DamkaBoard {
 					} else {
 						System.out.print("*");
 					}
-					check = false;
+
 				} else {
 
 					System.out.print("* ");
